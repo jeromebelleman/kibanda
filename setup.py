3760e8d2ed0eba9e4643bfbc8153186179d01edd
Jerome Belleman <jerome.belleman@gmail.com>
2017-05-09 19:54:51 +0200

    Package

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+import os
+from distutils.core import setup
+
+delattr(os, 'link')
+
+setup(
+    name='kibanda',
+    version='1.0',
+    author='Jerome Belleman',
+    author_email='Jerome.Belleman@gmail.com',
+    url='http://cern.ch/jbl',
+    description="Manage Kibana dashboards",
+    long_description="List, edit, back up Kibana dashboards.",
+    scripts=['kibanda'],
+    data_files=[],
+)
